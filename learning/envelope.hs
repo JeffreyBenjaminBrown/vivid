7566f277d14292893efe3838572b002b01ca69c6
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-07 19:23:20 -0500

    add type signatures so it will compile

--- learning/envelope.hs
+++ learning/envelope.hs
@@ -1,4 +1,4 @@
--- example by Tom Murphy:
+-- example by Tom Murphy: 
 -- https://we.lurk.org/hyperkitty/list/livecode@we.lurk.org/thread/ZQBFCHMBFIIM36KB7S77IDAPYJKMBRF2/
 
 {-# LANGUAGE DataKinds #-}
@@ -6,9 +6,15 @@
 import Vivid
 
 foo = sd (1 :: I "gate") $ do
-  e <- adsrGen 0.2 0.1 0.6 0.7 (Curve_Curve (-4)) (gate_ (V::V "gate"))
-  s <- e ~* sinOsc (freq_ $ midiCPS 50)
-  out 0 [s,s]
+  e <- adsrGen
+    (0.2::Double)  -- Float or Double works
+    (0.1::Double)  -- Float or Double works
+    (0.6::Double)  -- Float or Double works
+    (0.7::Double)  -- Float or Double works
+    (Curve_Curve $ -4)
+    (gate_ (V::V "gate"))
+  s <- e ~* sinOsc (freq_ (500 :: Double)) -- Float or Double works
+  out (0::Integer) [s,s]   -- Int or Integer works
 
 main = do
   s <- synth foo ()
