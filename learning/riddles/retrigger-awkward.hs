1629a628fc39d67b418f49f3ee5853513ccadcd3
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-08 14:14:58 -0500

    clarify: add comments, delete krufty code

--- learning/riddles/retrigger-awkward.hs
+++ learning/riddles/retrigger-awkward.hs
@@ -1,8 +1,10 @@
 -- Based on an example by Tom Murphy:
 -- https://we.lurk.org/hyperkitty/list/livecode@we.lurk.org/thread/ZQBFCHMBFIIM36KB7S77IDAPYJKMBRF2/
 
--- Retriggering an ADSR envelope is awkward.
--- (But maybe other envelopes are different?)
+-- Retriggering an ADSR envelope is awkward: You've got to close it,
+-- and then wait for at least half a milisecond, before it will rise
+-- above its sustaining level. 
+-- (Maybe other envelopes are different?)
 -- See comments in `main` for details.
 
 {-# LANGUAGE DataKinds, ExtendedDefaultRules, ViewPatterns #-}
@@ -16,16 +18,6 @@ foo = sd (1 :: I "gate") $ do
   s <- e ~* sinOsc (freq_ (500 ~+ (100 ~* e)))
   out 0 [s,s]
 
-mySong = doScheduledIn 0.1 $ do
-   fork $ do
-      wait 1
-      s1 <- synth foo ()
-      wait 1
-      free s1
-   s2 <- synth foo ()
-   wait 1.5
-   free s2
-
 test pauseLength = doScheduledIn 0.1 $ test' pauseLength
 
 test' pauseLength = do
@@ -34,8 +26,8 @@ test' pauseLength = do
 
   -- I can retrigger the AD phase, IF the R phase is allowed to progress
   -- for at least a milisecond. If the pause between the gate=0
-  -- and gate=1 messages is much less than that,
-  -- the AD phase is not retriggered; the synth stays in the R phase.
+  -- and gate=1 messages is substsantially less than that,
+  -- the AD phase is not retriggered; the synth stays in the S phase.
   -- (To see this, try running `test 0.001` and then `test 0.0001`.)
   set s (0 :: I "gate")
   wait pauseLength
