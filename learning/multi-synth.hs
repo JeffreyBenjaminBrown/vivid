eaf89631bfb1f4769ba5dc6557769a3a26355ea9
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-15 20:18:15 -0500

    multi-synth.hs: type safety + mixed message types

---
+++ learning/multi-synth.hs
@@ -0,0 +1,48 @@
+-- there's a more interesting synth in param-as-arg.hs
+
+{-# LANGUAGE DataKinds, ExtendedDefaultRules, GADTs #-}
+
+import Vivid
+import Data.List as L
+import Data.Map as M
+import GHC.TypeLits
+
+
+data Msg where
+  Msg :: (VarList usedParams, Subset (InnerVars usedParams) allParams)
+      => Synth allParams -> usedParams -> Msg
+
+send :: VividAction m => Msg -> m ()
+send (Msg synth params) = set synth params
+
+sendFree :: VividAction m => Msg -> m ()
+sendFree (Msg synth _) = free synth
+
+
+-- | = Synths
+
+bop :: SynthDef '["freq"]
+bop = sd ( 0 :: I "freq"
+         ) $ do
+   s1 <- 0.01 ~* sinOsc (freq_ (V::V "freq"))
+   out 0 [s1, s1]
+
+boop :: SynthDef '["freq","amp"]
+boop = sd ( 0    :: I "freq"
+          , 0.01 :: I "amp"
+          ) $ do
+   s1 <- (V::V "amp") ~* sinOsc (freq_ (V::V "freq"))
+   out 0 [s1, s1]
+
+
+-- | = main
+
+main = do
+  b <- synth bop ()
+  b' <- synth boop ()
+  let m  = Msg b  (toI 500 :: I "freq")
+      m' = Msg b' (toI 600 :: I "freq", toI 0.02 :: I "amp")
+  mapM_ send [m,m']
+  wait 1
+  mapM_ sendFree [m,m']
+
