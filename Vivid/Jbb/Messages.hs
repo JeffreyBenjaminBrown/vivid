2a48fed309b816df2de6f45cec543c1771e8725e
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-15 22:22:00 -0500

    simplify Msg class hide some kruft in a subfolder

--- Vivid/Jbb/Messages.hs
+++ Vivid/Jbb/Messages.hs
@@ -1,39 +1,15 @@
-{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DataKinds, ExtendedDefaultRules, GADTs #-}
 
 module Vivid.Jbb.Messages where
 
-import Data.List as L
-import Data.Map as M
-
 import Vivid
-import Vivid.Jbb.Synths
 
 
-type SynthModel = String
+data Msg where
+  ParamMsg :: (VarList usedParams, Subset (InnerVars usedParams) allParams)
+      => Synth allParams -> usedParams -> Msg
+  FreeMsg :: Synth params -> Msg
 
--- | A more recent attempt
-
-data Msg = MsgParamMsg ParamMsg | MsgFree
-data ParamMsg = ParamMsg { param :: String
-                         , value :: Float }
-
--- | An earlier attempt, which doesn't allow for targeting a specific
--- instance of a synth
-
-data Msg' =
-  Msg' { target :: SynthModel -- one synth model can have many instances
-      , param' :: String
-      , value' :: Float }
-
-data SynthModelMgr =
-  SynthModelMgr { free :: [Synth MyParams]
-                , busy :: [Synth MyParams] }
-
-type SynthMgr = M.Map SynthModel SynthModelMgr
-
-_sendMsg :: SynthModelMgr -> Msg' -> IO SynthModelMgr
-_sendMsg sm@(SynthModelMgr free busy) (Msg' _ param value) =
-  case free of [] -> do putStrLn "can't"
-                        return sm
-               f:fs -> do set' param value f
-                          return $ SynthModelMgr fs $ f:busy
+send :: VividAction m => Msg -> m ()
+send (ParamMsg synth params) = set synth params
+send (FreeMsg synth) = free synth
