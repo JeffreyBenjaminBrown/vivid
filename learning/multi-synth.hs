29e0ab4fbb3b8487504746b79b50637fa0ccbd1d
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-15 20:27:00 -0500

    make it more reader-friendly

--- learning/multi-synth.hs
+++ learning/multi-synth.hs
@@ -8,6 +8,8 @@ import Data.Map as M
 import GHC.TypeLits
 
 
+-- | = messages, which know where they're going
+
 data Msg where
   Msg :: (VarList usedParams, Subset (InnerVars usedParams) allParams)
       => Synth allParams -> usedParams -> Msg
@@ -19,7 +21,19 @@ sendFree :: VividAction m => Msg -> m ()
 sendFree (Msg synth _) = free synth
 
 
--- | = Synths
+-- | = main
+
+main = do
+  b <- synth bop ()
+  b' <- synth boop ()
+  let m  = Msg b  (toI 500 :: I "freq")
+      m' = Msg b' (toI 600 :: I "freq", toI 0.02 :: I "amp")
+  mapM_ send [m,m']
+  wait 1
+  mapM_ sendFree [m,m']
+
+
+-- | = synths
 
 bop :: SynthDef '["freq"]
 bop = sd ( 0 :: I "freq"
@@ -33,16 +47,4 @@ boop = sd ( 0    :: I "freq"
           ) $ do
    s1 <- (V::V "amp") ~* sinOsc (freq_ (V::V "freq"))
    out 0 [s1, s1]
-
-
--- | = main
-
-main = do
-  b <- synth bop ()
-  b' <- synth boop ()
-  let m  = Msg b  (toI 500 :: I "freq")
-      m' = Msg b' (toI 600 :: I "freq", toI 0.02 :: I "amp")
-  mapM_ send [m,m']
-  wait 1
-  mapM_ sendFree [m,m']
 
