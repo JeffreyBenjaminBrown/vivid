4a47591d7800a7187d6926f46c9ed264c0f60367
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-18 21:36:54 -0500

    add pseudocode for making the number-parsing more forgiving

--- Vivid/Jbb/ParseUtils.hs
+++ Vivid/Jbb/ParseUtils.hs
@@ -33,5 +33,16 @@ anyWord = L.lexeme sc $ some wordChar  <* notFollowedBy wordChar
 -- | Per this, negative signs must abut the numbers they negate.
 -- TODO: make it accept numbers without a decimal point, or with
 -- no digits to the right of it.
+  -- L.scientific doesn't accept those things either.
+  -- pseudocode:
+    -- parse the sign, or not, using option. return for that either 1 or -1.
+    -- parse an int, or not, using option
+    -- parse a period, or not, using option
+    -- parse an int, or not, using option
+    -- for each of those things that is missing, fill it with a default value
+          -- note that this way xthe sign is never missing
+    -- build a new number
+    -- parse it using a built-in function like L.float or L.scientific
+    -- multiply that by the sign
 signedFloat :: Parser Float
 signedFloat = L.signed nothing L.float where nothing = return ()
