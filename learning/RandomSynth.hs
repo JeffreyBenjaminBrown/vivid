bfdbfdd990d9a9bf3e91113380f1c1cfff1680d5
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-20 16:45:38 -0500

    toward generating random synthesizers

--- learning/RandomSynth.hs
+++ learning/RandomSynth.hs
@@ -2,7 +2,7 @@
 
 --module Vivid.Jbb.RandomSynth where
 --
---import Vivid
+import Vivid
 
 
 -- | Every random synth has up to eight parameters.
@@ -25,16 +25,55 @@ rSigName maxSigName n = if n <= min maxSigName 8 && n >= 1
   then [RS1, RS2, RS3, RS4, RS5, RS6, RS7, RS8] !! (n-1)
   else error $ show n ++ " is not the number of an RSigName."
 
-data RSig = RSig RSigName
+data RSig = RSig RSigName -- ^ a previously constructed RSig
           | RV RParam
           | RSigFormula RFormula
           | RSigGen RGen
+  deriving (Show, Eq, Ord)
 
 data RFormula = RProd RSig RSig
               | RSum RSig RSig
+  deriving (Show, Eq, Ord)
 
 data RGen = RSin RSinMsg
           | RSaw RSawMsg
+  deriving (Show, Eq, Ord)
 
 data RSinMsg = RSinMsg { rSinFreq :: RSig, rSinPhase :: RSig }
+  deriving (Show, Eq, Ord)
+
 data RSawMsg = RSawMsg { rSawFreq :: RSig }
+  deriving (Show, Eq, Ord)
+
+
+-- | = Generating random ones
+
+-- | without this we would usually create invalid signal graphs
+-- TODO ? How to ensure that previously created signals are actually used?
+data RPossible = RPossible { maxParams :: Int
+                           , nNamedSignals :: Int }
+
+rSawMsg :: IO RSawMsg -- TODO
+rSawMsg = return $ RSawMsg (RV RP1)
+
+rSaw :: IO RGen
+rSaw = RSaw <$> rSawMsg
+
+rSinMsg :: IO RSinMsg -- TODO
+rSinMsg = return $ RSinMsg (RV RP1) (RV RP2)
+
+rSin :: IO RGen
+rSin = RSin <$> rSinMsg
+
+rGen :: IO RGen
+rGen = do x <- pick [rSaw, rSin]
+          x -- this doesn't return `x`, it "does" `x`
+
+rFormula :: IO RFormula
+rFormula = do f <- pick [RProd, RSum]
+              a <- rSig
+              b <- rSig
+              return $ f a b
+
+rSig :: IO RSig -- TODO
+rSig = return $ RV RP1
