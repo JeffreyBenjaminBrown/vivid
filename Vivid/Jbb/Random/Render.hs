6d4140f30cf58afaef37d2f58ac748a4eb280dbd
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-21 02:40:04 -0500

    render AbParams to SDBodies!

--- Vivid/Jbb/Random/Render.hs
+++ Vivid/Jbb/Random/Render.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE DataKinds
            , ExtendedDefaultRules
            , FlexibleContexts
+           , ScopedTypeVariables
            , ConstrainedClassMethods
            #-}
 
@@ -19,15 +20,15 @@ import Vivid.Jbb.Random.RandomSynth
 type TheRParams = '["AP1", "AP2", "AP3", "AP4", "AP5", "AP6", "AP7", "AP8"]
 
 class RenderSig a where
-  renderSig :: ToSig a TheRParams
-            => (M.Map AbSigName a) -> a -> SDBody a Signal
+  renderSig :: M.Map AbSigName a -> a -> SDBody' TheRParams Signal
 
---instance RenderSig AbParam where
---  renderSig _ AP1 = (V :: V "AP1")
---  renderSig _ AP2 = (V :: V "AP2")
---  renderSig _ AP3 = (V :: V "AP3")
---  renderSig _ AP4 = (V :: V "AP4")
---  renderSig _ AP5 = (V :: V "AP5")
---  renderSig _ AP6 = (V :: V "AP6")
---  renderSig _ AP7 = (V :: V "AP7")
---  renderSig _ AP8 = (V :: V "AP8")
+instance RenderSig AbParam where
+  -- This is a goofy hack, but it's cpu-cheap.
+  renderSig _ AP1 = (V :: V "AP1") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP2 = (V :: V "AP2") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP3 = (V :: V "AP3") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP4 = (V :: V "AP4") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP5 = (V :: V "AP5") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP6 = (V :: V "AP6") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP7 = (V :: V "AP7") ~+ (0 ~* pulse (freq_ 1))
+  renderSig _ AP8 = (V :: V "AP8") ~+ (0 ~* pulse (freq_ 1))
