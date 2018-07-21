e8a41ba9077c01ef8a374bd4d57396139ed4895a
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-20 23:40:20 -0500

    Prune an abstract synth!

--- Vivid/Jbb/Random/Synth.hs
+++ Vivid/Jbb/Random/Synth.hs
@@ -1,6 +1,6 @@
 module Vivid.Jbb.Random.Synth where
 
-import Data.Map as M
+import qualified Data.Map as M
 
 import Vivid
 import Vivid.Jbb.Random.Signal
@@ -10,7 +10,8 @@ import Vivid.Jbb.Random.MentionsSig
 type AbSynth = M.Map AbSigName AbSig
 
 randAbSynth :: RandConstraints -> IO AbSynth
-randAbSynth cs = go cs M.empty where
+randAbSynth cs = prune cs <$>
+  go cs M.empty where
   go :: RandConstraints -> AbSynth -> IO AbSynth
   go cs m = if namedSignals cs >= maxSignals cs
             then return m
@@ -20,5 +21,25 @@ randAbSynth cs = go cs M.empty where
                         m' = M.insert (sigName cs' namedSignals') s m
                     go cs' m'
 
---prune :: AbSynth -> AbSynth
---prune m = 
+-- | After pruning, every remaining signal influences the last one
+prune :: RandConstraints -> AbSynth -> AbSynth
+prune cs m =
+  let theUnused = unused cs [maximum $ M.keys m] m
+      deleteKeys :: Ord k => [k] -> M.Map k a -> M.Map k a
+      deleteKeys ks m = foldl (flip M.delete) m ks
+  in deleteKeys (M.keys theUnused) m
+
+-- | Produces an AbSynth containing only the unused signals
+unused :: RandConstraints -> [AbSigName] -> AbSynth -> AbSynth
+unused cs [] all = all
+unused cs (u:used) m = 
+  let newMentions = allMentions cs u
+      m' = M.delete u m
+      remainingLeads = unique
+        $ filter (flip elem $ M.keys m) -- delete irrelevant keys
+        $ newMentions ++ used
+  in unused cs remainingLeads m'
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (a:as) = a : (unique $ filter (not . (==) a) as)
