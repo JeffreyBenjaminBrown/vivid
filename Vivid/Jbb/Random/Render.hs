1251626cd85e0060e7fa7bec29bd0d850ed5a8fd
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-21 23:47:20 -0500

    nothing

--- Vivid/Jbb/Random/Render.hs
+++ Vivid/Jbb/Random/Render.hs
@@ -33,21 +33,22 @@ instance RenderSig AbFormula where
   renderSig (AbSum x y) m = renderSig x m ~+ renderSig y m
 
 instance RenderSig AbGen where
-  renderSig (AbSin (AbSinMsg freq phase)) = \m -> 
-    sinOsc (freq_ $ renderSig freq m, phase_ $ renderSig phase m)
-  renderSig (AbSaw (AbSawMsg freq)) = \m -> 
-    saw (freq_ $ renderSig freq m)
+  renderSig (AbSin (AbSinMsg freq phase)) m = 
+    sinOsc ( freq_ $ renderSig freq m
+           , phase_ $ renderSig phase m )
+  renderSig (AbSaw (AbSawMsg freq)) m =
+    saw    ( freq_ $ renderSig freq m )
 
 instance RenderSig AbSigName where
   renderSig name = \m -> toSig $ (M.!) m name
     -- confusingly, toSig converts a signal to an SDBody'
 
 instance RenderSig AbParam where
-  renderSig AP1 = const $ toSig (V :: V "AP1")
-  renderSig AP2 = const $ toSig (V :: V "AP2")
-  renderSig AP3 = const $ toSig (V :: V "AP3")
-  renderSig AP4 = const $ toSig (V :: V "AP4")
-  renderSig AP5 = const $ toSig (V :: V "AP5")
-  renderSig AP6 = const $ toSig (V :: V "AP6")
-  renderSig AP7 = const $ toSig (V :: V "AP7")
-  renderSig AP8 = const $ toSig (V :: V "AP8")
+  renderSig AP1 _ = toSig (V :: V "AP1")
+  renderSig AP2 _ = toSig (V :: V "AP2")
+  renderSig AP3 _ = toSig (V :: V "AP3")
+  renderSig AP4 _ = toSig (V :: V "AP4")
+  renderSig AP5 _ = toSig (V :: V "AP5")
+  renderSig AP6 _ = toSig (V :: V "AP6")
+  renderSig AP7 _ = toSig (V :: V "AP7")
+  renderSig AP8 _ = toSig (V :: V "AP8")
