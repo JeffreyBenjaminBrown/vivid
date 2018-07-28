786fffe4d38a2c11e512a1d168cce6cb01eca205
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-07-28 12:17:28 -0500

    zot: add sin as well as feedback to drive wm, pm, fm

--- learning/zot.hs
+++ learning/zot.hs
@@ -1,29 +1,49 @@
--- This is the preferred feedback method; for some reason using localIn
--- and localOut sounds better than using localBuf, playBuf and recordBuf.
--- Notice that there can only be one (audio rate) localIn/localOut pair
--- per synthdef. That's no problem, because it can have lots of channels.
+-- next: add AM, RM, filter, shaper, delay, pitch shift.
+-- then ? taps
 
 {-# LANGUAGE DataKinds, ExtendedDefaultRules #-}
 
 import Vivid
 
 
-type ZotParams = '["freq","amp","pulse","fm","del"]
+type ZotParams = '["amp"
+  ,"pulse"                    -- pulse + sin = 1
+  ,"f","fm-b","fm-m","fm-f"  -- baseline, fb mul, sin mul, sin freq
+      ,"pm-b","pm-m","pm-f"            -- fb mul, sin mul, sin freq
+  ,"w","wm-b","wm-m","wm-f"  -- baseline, fb mul, sin mul, sin freq
+  ,"del"]
 
 zot :: SynthDef ZotParams
-zot = sd ( 0 :: I "freq"
-         , 0 :: I "amp"
-         , 0 :: I "pulse"
-         , 0 :: I "fm"
+zot = sd ( 0 :: I "amp"
+         , 0.5 :: I "pulse"
+         , 0 :: I "f"
+         , 0 :: I "fm-b"
+         , 0 :: I "fm-m"
+         , 0 :: I "fm-f"
+         , 0 :: I "pm-b"
+         , 0 :: I "pm-m"
+         , 0 :: I "pm-f"
+         , 0.5 :: I "w"
+         , 0 :: I "wm-b"
+         , 0 :: I "wm-m"
+         , 0 :: I "wm-f"
          , 0.01 :: I "del"
          ) $ do
   [fb_1] <- localIn(1)
-  fm <- (V::V "freq") ~+ ((V::V "fm") ~* fb_1)
+  fb01 <- ((fb_1 ~+ 1) ~/ 2) -- varies in [0,1]
+  fm <- (V::V "f")
+        ~+ (V::V "fm-b") ~* fb_1
+        ~+ (V::V"fm-m") ~* sinOsc (freq_ (V::V"fm-f"))
+  pm <- (pi/2) ~* (   (V::V"pm-b") ~* fb01
+                   ~+ (V::V"pm-m") ~* sinOsc (freq_ (V::V"pm-f")))
+  wm <- (V :: V "w")
+    ~+ 0.5 ~* (   (V::V"wm-b") ~* fb_1
+               ~+ (V::V"wm-m")  ~* sinOsc (freq_ (V::V"wm-f")))
 
-  aSin <- sinOsc (freq_ fm)
-  aPulse <- pulse (freq_  fm)
-  source <- (V::V "amp") ~* (    (      (V :: V "pulse" ) ~* aPulse)
-                              ~+ ((1 ~- (V :: V "pulse")) ~* aSin) )
+  aSin <- sinOsc  (freq_ fm, phase_ pm)
+  aPulse <- pulse (freq_ fm, width_ wm)
+  source <-          (V :: V "pulse" ) ~* aPulse
+            ~+ (1 ~- (V :: V "pulse")) ~* aSin
 
   s1 <- delayL( in_ source
                , maxDelaySecs_ 1
@@ -31,4 +51,4 @@ zot = sd ( 0 :: I "freq"
 
   localOut( [s1] )
 
-  out 0 [source,source]
+  out 0 [(V::V "amp") ~* source, (V::V "amp") ~* source]
--- learning/zot.params.hs
+++ learning/zot.params.hs
@@ -1,5 +1,12 @@
+s <- synth zot ()
+set s (0.1 :: I "amp", (1/300) :: I "del", 0.5 :: I "pulse")
+set s               (1 :: I "pm-b", 1 :: I "pm-m", 1 :: I "pm-f")
+set s (400 :: I "f", 3 :: I "fm-b", 30 :: I "fm-m", 300 :: I "fm-f")
+set s (0.5 :: I "w", 1 :: I "wm-b", 30 :: I "wm-m", 300 :: I "wm-f")
 
-set s (400 :: I "freq",0.1 :: I "amp")
-set s ((1/300) :: I "del", 1000 :: I "fm")
-set s (0.5 :: I "pulse")
-set s (0 :: I "fm")
+t <- synth zot ()
+set t (0.1 :: I "amp", (1/300) :: I "del", 0.5 :: I "pulse")
+set t               (1 :: I "pm-b", 1 :: I "pm-m", 1 :: I "pm-f")
+set t (400 :: I "f", 3 :: I "fm-b", 30 :: I "fm-m", 300 :: I "fm-f")
+set t (0.5 :: I "w", 1 :: I "wm-b", 30 :: I "wm-m", 300 :: I "wm-f")
+
