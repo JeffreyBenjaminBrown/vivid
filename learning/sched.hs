4504df60bb8d031b85ffde5e1e2b0c052243136c
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-04 11:01:56 -0500

    learn to change tempo of a loop

--- learning/sched.hs
+++ learning/sched.hs
@@ -1,6 +1,7 @@
-import Vivid
+import Vivid hiding (next)
 import Vivid.OSC
 import Control.Concurrent (forkIO, killThread)
+import Control.Concurrent.MVar
 
 
 unTimestamp :: Timestamp -> Double
@@ -27,3 +28,37 @@ loop = do
   x <- forkIO loop 
   getChar
   killThread x
+
+next :: RealFrac a => a -> a -> a -> a
+next time0 tempo now =
+  fromIntegral (ceiling $ (now - time0) / tempo ) * tempo + time0
+
+-- | If you execute the following commands, with a good wait in between each:
+-- (l,ch) <- tempo
+-- ch 0.5
+-- killThread l
+-- you should see a print statement looped with an initial tempo of 1 Hz,
+-- which changes to 2 Hz when you run "ch 0.5".
+tempo = do
+  time0 <- (\x -> x-0.05) . unTimestamp <$> getTime >>= newMVar
+    -- subtracting .1 makes it start in .05 seconds rather than 1 second
+  tempo <- newMVar 1
+  let loop = do
+        now <- unTimestamp <$> getTime
+
+        -- changes to time0 or tempo won't affect this cycle
+        time0' <- readMVar time0
+        tempo' <- readMVar tempo
+
+        let nextCycle = next time0' tempo' now
+        wait $ nextCycle - now
+        print $ show now
+        loop
+  let chTempo :: Double -> IO ()
+      chTempo newTempo = do now <- unTimestamp <$> getTime
+                            time0_value <- readMVar time0
+                            tempo_value <- readMVar tempo
+                            swapMVar time0 $ next time0_value tempo_value now
+                            swapMVar tempo newTempo >> return ()
+  l <- forkIO loop
+  return (l,chTempo)
