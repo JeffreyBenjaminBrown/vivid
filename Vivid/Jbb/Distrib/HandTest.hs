0c39fe6815e0319d99d49984c9de968ce6e1b9ac
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-06 00:02:47 -0500

    hand testing

--- Vivid/Jbb/Distrib/HandTest.hs
+++ Vivid/Jbb/Distrib/HandTest.hs
@@ -6,16 +6,24 @@ import qualified Data.Vector as V
 
 import Vivid
 import Vivid.Jbb.Synths
+import Vivid.Jbb.Distrib.Act
 import Vivid.Jbb.Distrib.Distrib
 import Vivid.Jbb.Distrib.Types
 
 
+msq = Museq {_dur = 2,
+              _vec = V.fromList [(0, New Boop "marge")
+                                ,(0.1, Send Boop "marge" ("freq",444))
+                                ,(0.1, Send Boop "marge" ("amp",0.2))
+                                ,(0.5, Free Boop "marge")] }
+
+dunno = do
+  dist <- newDistrib
+  act (reg dist) $ snd $ (V.!) (_vec msq) 0
+
+-- | call like this:
+-- tid <- handTest
 handTest = do
   dist <- newDistrib
-  let msq = Museq {_dur = 2,
-                   _vec = V.fromList [(0, New Boop "marge")
-                                     ,(0, Send Boop "marge" ("freq",444))
-                                     ,(0, Send Boop "marge" ("amp",0.2))
-                                     ,(0.5, Free Boop "marge")] }
   swapMVar (mTimeMuseqs dist) $ M.fromList [("1",(0::Time, msq))]
   startDistribLoop dist
