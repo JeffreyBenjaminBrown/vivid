1b49e77fa3a73ffb62902714669d0343187716dd
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-06 00:50:24 -0500

    futz with tests, write to org-mode

--- Vivid/Jbb/Distrib/HandTest.hs
+++ Vivid/Jbb/Distrib/HandTest.hs
@@ -17,13 +17,22 @@ msq = Museq {_dur = 2,
                                 ,(0.1, Send Boop "marge" ("amp",0.2))
                                 ,(0.5, Free Boop "marge")] }
 
-dunno = do
+actIsWorking = do
   dist <- newDistrib
-  act (reg dist) $ snd $ (V.!) (_vec msq) 0
+  act (reg dist) $ New Boop "fred"
+  putStrLn =<< show <$> readMVar ( boops $ reg $ dist)
+    -- `act` called on a `New` indeed creates that synth
 
--- | call like this:
--- tid <- handTest
-handTest = do
+  act (reg dist) $ Send Boop "fred" ("freq", 444)
+  act (reg dist) $ Send Boop "fred" ("amp", 0.1)
+
+  wait 1
+  act (reg dist) $ Free Boop "fred"
+
+-- | Call it, and then end it, like this:
+-- tid <- loopTest
+-- killThread tid
+loopTest = do
   dist <- newDistrib
   swapMVar (mTimeMuseqs dist) $ M.fromList [("1",(0::Time, msq))]
   startDistribLoop dist
