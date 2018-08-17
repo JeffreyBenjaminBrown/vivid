78d0b7b080946c81748573201963ebdaba6a6627
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-17 10:33:12 -0500

    dispatchLoop3: works!

---
+++ music/terse3.hs
@@ -0,0 +1,27 @@
+a1 = museq 1 [ (0,   Send Boop "1" ("freq",400) )
+             , (0,   Send Boop "1" ("amp",0.4)  )
+             , (0.5, Send Boop "1" ("amp",0)    ) ]
+m1 = museq 2 [ (0,   ("amp",0)    )
+             , (0.5, ("freq",500) )
+             , (0.5, ("amp",0.4)  ) ]
+a2 = Send Boop "2" <$> m1
+a3 = fast 2 $ early (1/4) $ museq 1 [ (0,   Send Boop "3" ("freq",600) )
+                                    , (0,   Send Boop "3" ("amp",0.4)  )
+                                    , (0.5, Send Boop "3" ("amp",0)    ) ]
+
+dist <- newDispatch3
+swapMVar (mTimeMuseqs3 dist) $ M.fromList [ ("1",a1), ("2",a3)]
+
+s <- synth boop ()
+t <- synth boop ()
+u <- synth boop ()
+swapMVar (mReg3 dist) $ SynthRegister3 {
+  boops3 = M.fromList [("1",s),("2",t),("3",u)]
+  , vaps3 = M.empty
+  , sqfms3 = M.empty }
+
+-- replaceAll3 dist $ M.fromList [ ("1",a1), ("2",a3)]
+-- replace dist "2" a3
+
+tid <- startDispatchLoop3 dist
+putStrLn "dist <- newDispatch\ntid <- startDispatchLoop dist"
