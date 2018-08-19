76350f67e7c60e5d3ac552547504341882b81e78
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-18 20:30:58 -0500

    bury some types used only in buried code

--- Vivid/Jbb/Dispatch/Types.hs
+++ Vivid/Jbb/Dispatch/Types.hs
@@ -20,17 +20,11 @@ module Vivid.Jbb.Dispatch.Types (
   , emptyMuseq
   , unitMuseq
   , museq
-  , SynthRegister(..)
-  , emptySynthRegister
-  , showSynthRegister
   , SynthRegister3(..), boops3, vaps3, sqfms3
   , emptySynthRegister3
-  , Dispatch(..)
-  , newDispatch
   , Dispatch3(..)
   , newDispatch3
   , Msg'(..)
-  , Action'(..)
   ) where
 
 import Control.Concurrent.MVar
@@ -110,20 +104,6 @@ museq d tas = Museq {_dur = d, _sup = d, _vec = V.fromList tas}
 
 -- | The global state
 
-data SynthRegister = -- per-synth boilerplate
-  SynthRegister { boops :: MVar (M.Map SynthName (Synth BoopParams))
-                , vaps  :: MVar (M.Map SynthName (Synth VapParams))
-                , sqfms :: MVar (M.Map SynthName (Synth SqfmParams))
-                -- , zots :: MVar (M.Map SynthName (Synth ZotParams))
-                }
-
-emptySynthRegister :: IO SynthRegister
-emptySynthRegister = do x <- newMVar M.empty
-                        y <- newMVar M.empty
-                        z <- newMVar M.empty
---                        w <- newMVar M.empty
-                        return $ SynthRegister x y z -- w
-
 data SynthRegister3 = -- per-synth boilerplate
   SynthRegister3{ _boops3 :: M.Map SynthName (Synth BoopParams)
                 , _vaps3  :: M.Map SynthName (Synth VapParams)
@@ -135,32 +115,6 @@ makeLenses ''SynthRegister3
 emptySynthRegister3 :: SynthRegister3
 emptySynthRegister3 = SynthRegister3 M.empty M.empty M.empty
 
-
--- | todo : this blocks if any MVar is empty
-showSynthRegister :: SynthRegister -> IO String
-showSynthRegister reg = do bs <- show <$> (readMVar $ boops reg)
-                           vs <- show <$> (readMVar $ vaps reg )
-                           ss <- show <$> (readMVar $ sqfms reg)
-                           return $ bs ++ "\n" ++ vs ++ "\n" ++ ss
-
-data Dispatch = Dispatch {
-  mTimeMuseqs :: MVar (M.Map MuseqName (Time, Museq Action))
-    -- ^ Each `Time` here is the next time that Museq is scheduled to run.
-    -- Rarely, briefly, those `Time` values will be in the past.
-  , reg :: SynthRegister
-  , mTime0 :: MVar Time
-  , mTempoPeriod :: MVar Duration
-  }
-
--- | "new" because it's not really empty, except for `time0`
-newDispatch :: IO Dispatch
-newDispatch = do
-  mTimeMuseqs <- newMVar M.empty
-  reg <- emptySynthRegister
-  mTime0 <- newEmptyMVar
-  mTempoPeriod <- newMVar 1
-  return Dispatch { mTimeMuseqs = mTimeMuseqs,  reg          = reg
-                  , mTime0  = mTime0         ,  mTempoPeriod = mTempoPeriod }
 
 data Dispatch3 = Dispatch3 {
   mMuseqs3 :: MVar (M.Map MuseqName (Museq Action))
@@ -188,13 +142,3 @@ data Msg' sdArgs where
           ( VarList params
           , Subset (InnerVars params) sdArgs)
        => params -> Msg' sdArgs
-
-data Action' where
-  New'  :: MVar (M.Map SynthName (Synth sdArgs))
-        -> SynthDef sdArgs
-        -> SynthName -> Action'
-  Free' :: MVar (M.Map SynthName (Synth sdArgs))
-        -> SynthName -> Action'
-  Send' :: MVar (M.Map SynthName (Synth sdArgs))
-        -> SynthName
-        -> Msg' sdArgs -> Action'
