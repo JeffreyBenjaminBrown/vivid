8cc04185cba0e26c2c335dde3fbf823e3824f796
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-24 16:04:54 -0500

    make easier to hear

--- music/1.hs
+++ music/1.hs
@@ -1,16 +1,17 @@
-a1 = museq' 1 [ (0,   Send Boop "1" ("freq",400) )
+a1 = museq' 1 [ (0,   Send Boop "1" ("freq",1400) )
               , (0,   Send Boop "1" ("amp",0.4)  )
               , (0.5, Send Boop "1" ("amp",0)    ) ]
 m1 = museq' 2 [ (0,   ("amp",0)    )
-              , (0.5, ("freq",500) )
+              , (0.5, ("freq",1500) )
               , (0.5, ("amp",0.4)  ) ]
 a2 = Send Boop "2" <$> m1
-a3 = fast 2 $ early (1/4) $ museq' 1 [ (0,   Send Boop "3" ("freq",600) )
+a3 = fast 2 $ early (1/4) $ museq' 1 [ (0,   Send Boop "3" ("freq",800) )
                                      , (0,   Send Boop "3" ("amp",0.4)  )
                                      , (0.5, Send Boop "3" ("amp",0)    ) ]
 
 disp <- newDispatch
 swapMVar (mMuseqs disp) $ M.fromList [ ("1",fast 11 a1), ("2",fast 2 a3)]
+swapMVar (mTempoPeriod disp) 2
 
 s <- synth boop ()
 t <- synth boop ()
@@ -21,7 +22,7 @@ swapMVar (mReg disp) $ SynthRegister {
   , _sqfms = M.empty }
 
 -- replaceAll disp $ M.fromList [ ("1",fast 3 a1), ("2",fast 2 a2)]
--- replaceAll disp $ M.fromList [ ("1",fast 4 a1), ("2",fast 1.5 a3)]
+-- replaceAll disp $ M.fromList [ ("1",early 1 $ fast 4 a1), ("2",rev $ fast 1.5 a3)]
 -- replace disp "2" a3
 
 -- TODO ! bug, freezes synths mid-note
