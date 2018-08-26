74faadde32c0864053fded4b151aa9b7c58ee784
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-08-25 19:34:41 -0500

    find a bug replace an obsolete file with its updated version

--- music/1.hs
+++ music/1.hs
@@ -1,17 +1,17 @@
-a1 = museq' 1 [ (0,   Send Boop "1" ("freq",1400) )
-              , (0,   Send Boop "1" ("amp",0.4)  )
-              , (0.5, Send Boop "1" ("amp",0)    ) ]
-m1 = museq' 2 [ (0,   ("amp",0)    )
-              , (0.5, ("freq",1500) )
-              , (0.5, ("amp",0.4)  ) ]
+a1 = museq' 1 [ (0,   Send Boop "1" $ M.fromList [ ("freq",1400)
+                                                    , ("amp",0.4) ] )
+              , (0.5, Send Boop "1" $ M.singleton "amp" 0) ]
+m1 = museq' 2 [ (0,   M.fromList $ [ ("amp",0)
+                                   , ("freq",1500) ] )
+              , (0.5, M.singleton "amp" 0.4 ) ]
 a2 = Send Boop "2" <$> m1
-a3 = fast 2 $ early (1/4) $ museq' 1 [ (0,   Send Boop "3" ("freq",800) )
-                                     , (0,   Send Boop "3" ("amp",0.4)  )
-                                     , (0.5, Send Boop "3" ("amp",0)    ) ]
+a3 = fast 2 $ early (1/4)
+  $ museq' 1 [ (0,   Send Boop "3" $ M.fromList [ ("freq",800)
+                                                   , ("amp",0.4) ] )
+             , (0.5, Send Boop "3" $ M.singleton "amp" 0 ) ]
 
 disp <- newDispatch
-swapMVar (mMuseqs disp) $ M.fromList [ ("1",fast 11 a1), ("2",fast 2 a3)]
-swapMVar (mTempoPeriod disp) 2
+-- swapMVar (mTempoPeriod disp) 2
 
 s <- synth boop ()
 t <- synth boop ()
@@ -24,12 +24,9 @@ swapMVar (mReg disp) $ SynthRegister {
 -- replaceAll disp $ M.fromList [ ("1",fast 3 a1), ("2",fast 2 a2)]
 -- replaceAll disp $ M.fromList [ ("1",early 1 $ fast 4 a1), ("2",rev $ fast 1.5 a3)]
 -- replace disp "2" a3
-
--- TODO ! bug, freezes synths mid-note
 -- replaceAll disp $ M.fromList [ ("2",fast 3 a3)]
-
--- testing chTempoPeriod
 -- replaceAll disp $ M.fromList [ ("1", a1), ("2",  a2)]
+
 -- chTempoPeriod disp 1.05
 
 tryReadMVar $ mMuseqs disp
