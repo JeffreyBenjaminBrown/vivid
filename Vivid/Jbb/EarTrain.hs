a55e8d9ed1ccdef77686fffe7c86091eb2e450f0
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-09-25 00:47:04 -0500

    comments

--- Vivid/Jbb/EarTrain.hs
+++ Vivid/Jbb/EarTrain.hs
@@ -1,4 +1,4 @@
--- | = How to use the ear training function here.
+-- | = How to run the ear training functions here.
 --
 -- (0) You'll need Stack and SuperCollider installed.
 --     (I believe using Stack is both easier and safer than using a global
@@ -36,6 +36,125 @@
 -- (5) Follow the on-screen prompts.
 
 
+-- | A study plan: the Victor Burges ear-training program
+--
+-- When I was in high school, I read an ad in Guitar Magazine for a set of
+-- ear training tapes by Victor Burges. Over months I scrounged up $100 
+-- and bought them. They worked like a dream: 
+-- after two months of training maybe 20 minutes a day,
+-- I could hear a new melody and know how to play it.
+--
+-- If I recall correctly, Burges first taught perfect fifths.
+-- First he would assign homework: You had to find a piano (or something),
+-- and play C and G, and sing them. (Or hum, or whistle; whatever.)
+-- Then sing C# and G#, then D and A, etc.,
+-- all the way up and down an octave. Next do the same thing, only
+-- singing the higher note first. Let's call that whole routine
+-- "singing an octave of the interval". (Just like a bodybuilder,
+-- it's good to mix up the order of operations, so maybe sometimes start
+-- singing descending intervals instead of ascending ones.)
+--
+-- Next, Burges would test whether you had absorbed the interval:
+-- He would play a note and ask you to sing a fifth above or below.
+-- This part of the Burges tapes was kind of a waste of money, because
+-- it's something you can do yourself: Just play a note at random,
+-- try to sing a fifth above or below, then play the "answer" and see if
+-- you were right. Let's call that test "producing the interval".
+--
+-- If you couldn't pass the "producing the interval" test, you had to
+-- sing another octave of the interval. Cycle through those two steps
+-- until you pass the second one.
+--
+-- Now you know perfect fifths! Next, do the same for fourths: Sing an
+-- octave of fourths (ascending and descending), then test yourself at
+-- producing fourths.
+--
+-- Now that you know two intervals, Vivid.EarTrain becomes useful:
+-- Burge's next step was to ask you to distinguish between fourths and fifths.
+-- You can ask this software to do that, by running
+-- `earTrainFromChordList [[0,7],[0,5]]`
+--
+-- If I recall correctly, the rest of the Burges program was:
+--
+-- add the major third (the interval [0,4]:
+--   (1) sing an octave of major thirds
+--   (2) test yourself at producing major thirds
+--   (3) `earTrainFromChordList [[0,7],[0,5],[0,4]]`
+--
+-- add the minor third (the interval [0,3]):
+--   (1) sing an octave of minor thirds (the interval [0,3])
+--   (2) test yourself at producing minor thirds
+--   (3) `earTrainFromChordList [[0,7],[0,5],[0,4],[0,3]]`
+--
+-- (the same drill applies to every new interval:
+--   sing an octave, test producing it, then add it to the list.)
+--
+-- add the major sixth = [0,9]
+-- add the minor sixth = [0,8]
+-- add the minor seventh = [0,10]
+-- add the major seventh = [0,11]
+-- add the major second = [0,2]
+-- add the minor second = [0,1]
+-- add the tritone = diminished fifth = augmented fourth = [0,6]
+--
+-- Now you know all the intervals! Those are roughly ordered by increasing
+-- difficulty.
+--
+-- An advantage of this software over the audio tapes of my youth is that
+-- if you find yourself having trouble distinguishing two (or a few) sounds,
+-- you can focus on exactly those sounds. For instance, suppose you've
+-- almost completed the above program: you're testing yourself on all
+-- the intervals:
+-- `earTrainFromChordList $ (\x -> [0,x]) <$> [1..11]`
+-- and you keep confusing minor sevenths for major seconds. You can put
+-- the giant test on hold to focus on only those two problem intervals:
+-- `earTrainFromChordList [[0,2],[0,10]]`
+-- Similarly, if you yourself always nailing a certain kind of question,
+-- you can remove it from the set of things you're being tested on, to
+-- save time.
+-- 
+-- Here are the triads, again roughly in order of increasing difficulty:
+-- major = [0,4,7]
+-- minor = [0,3,7]
+-- diminished = [0,3,6]
+-- augmented = [0,4,8]
+-- You don't have to learn all the intervals before you start learning
+-- the triads. I think Burges introduced major and minor chords around
+-- the same time as major and minor thirds, maybe even before.
+--
+-- Here are the common 7th chords, again in roughly increasing difficulty:
+-- dom 7 = [0,4,7,10]
+-- min 7 = [0,3,7,10]
+-- maj 7 = [0,4,7,11]
+-- (fully) diminished 7 = [0,3,6,9]
+-- half-diminished 7 = [0,3,6,10]
+-- aug 7 = [0,4,8,10]
+-- min maj 7 = [0,3,7,11]
+-- aug maj 7 = [0,4,8,11]
+--
+-- One minor complication is that chords can be played in "inverted position",
+-- where some notes are displaced by one or more octaves from where they lie
+-- in the standard "close position". This software doesn't have a mechanism
+-- for handling inversions (yet). If you want, you can work with them by hand.
+-- For instance, here are all the inversions of a major triad:
+-- [0,4,7] -- Major triad, "root position"
+-- [4,7,12] -- Major triad, "first inversion"
+-- [7,12,16] -- Major triad, "second inversion"
+-- You could also put the chords in "open psition":
+-- [0,7,16] -- A wider major triad in root position
+-- [4,12,19] -- A wider major triad in first inversion (the inversion is
+--              named based only on the note in the root)
+-- [7,16,24] -- A wider major triad in second inversion
+-- [0,16,31] -- An extremely wide major triad ...
+-- The reason I call inversions a "minor" complication is that once you learn
+-- to recognize the chords in root position, you've done almost all the work.
+-- You'll find yourself recognizing inverted chords even before you learn
+-- how to recognize which inversion it is.
+--
+-- This long comment only describes using `earTrainFromChordList`, but
+-- all the functions that begin with earTrain are useful.
+
+
 {-# LANGUAGE ScopedTypeVariables
 , DataKinds
 , LambdaCase
@@ -60,33 +179,35 @@ type Test = (PlayQuestion, ShowAnswer)
 -- | quizzes from subsets drawn uniformly from a chromatic range
 earTrainChromatic :: Int -> Int -> IO ()
 earTrainChromatic numberOfFreqs range =
-  earTrain $ pickChromaticTest numberOfFreqs range
+  runEarTests $ pickChromaticTest numberOfFreqs range
 
 -- | like earTrainChromatic, but no 3 consecutive chromatically adjacent tones
 earTrain3ClusterFreeChromatic :: Int -> Int -> IO ()
 earTrain3ClusterFreeChromatic numberOfFreqs range =
-  earTrain $ pick3ClusterFreeTest numberOfFreqs range
+  runEarTests $ pick3ClusterFreeTest numberOfFreqs range
 
+-- | e.g. `earTrainFromScale 3 [0,2,4,6,8,10]` will quiz you on
+-- 3 note subsets of the whole tone scale.
 earTrainFromScale :: Int -> [Float] -> IO ()
 earTrainFromScale numberOfFreqs scale =
-  earTrain $ pickTestFromScale scale numberOfFreqs
+  runEarTests $ pickTestFromScale scale numberOfFreqs
 
 -- | quizzes from a list of chords
 earTrainFromChordList :: [[Float]] -> IO ()
-earTrainFromChordList chords = earTrain $ pickTestFromChordList chords
+earTrainFromChordList chords = runEarTests $ pickTestFromChordList chords
 
 
 -- | = The top IO workhorse
 
-earTrain :: IO Test -> IO ()
-earTrain pickTest = pickTest >>= runTest where
+runEarTests :: IO Test -> IO ()
+runEarTests pickTest = pickTest >>= runTest where
   runTest :: Test -> IO ()
   runTest test@(playFreqs, showFreqs) = do
     showChoices
     playFreqs
     getChar >>= \case
       'a' -> putStrLn "nother sound!\nBTW, the last sound was:"
-             >> showFreqs >> earTrain pickTest
+             >> showFreqs >> runEarTests pickTest
       's' -> putStrLn "how what was played" >> showFreqs
              >> runTest test
       'q' -> putStrLn "uit" >> return ()
