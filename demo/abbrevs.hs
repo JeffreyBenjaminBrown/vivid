4c4effe274676398b8632fa1085e63148b98de97
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-04-25 12:51:37 -0500

    comments

--- demo/abbrevs.hs
+++ demo/abbrevs.hs
@@ -1,14 +1,18 @@
-p f g = mmho 3 $ pre2 "a"
+p f g = mmho 3 $ -- mmho = make museq' + holds + ons
+                 -- 'h': hold each event until the next
+                 -- 'o': insert "on=1" wherever "on" isn't 0
+  pre2 "a" -- to each of these pairs, prefix "a"
   [ (0, m1 "freq" f)
   , (1, m1 "freq" $ f*g)
   , (2, m1 "on" 0) ]
 
-m = mmh 9
+m = mmh 9 -- mmh = make museq' + holds
+-- (ons only makes sense for Msg payloads; these are functions)
   [ trip "a" 0 id
   , trip "a" 3 $ fast' 2
   , trip "b" 3 $ fast' 4
   -- Wart: For safety, to prevent hanging notes,
-  -- both voices must be turned off explicitly, I think.
+  -- all voices must be turned off explicitly.
   , trip "a" 7 $ offs
   , trip "b" 7 $ offs
   ]
