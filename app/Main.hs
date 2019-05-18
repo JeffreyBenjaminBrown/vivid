83753db28a933fd435ab98adeae6fb458ac49ce3
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2019-05-18 16:55:28 -0500

    profiling! see TODO.org disable Hode, Random, EarTrain simplify the executable

--- app/Main.hs
+++ app/Main.hs
@@ -14,18 +14,14 @@ import VividDispatch
 
 main :: IO ()
 main = do
+  putStrLn $ "\\nPress any key to quit. " ++
+    "(You'll probably want to wait until you hear something first.)"
   disp <- newDispatch
   tid <- startDispatchLoop disp
 
-  let -- useful for live coding, but unused in this program
-      -- ch = replace disp               -- change one thing
-      -- stop = stopDispatch disp        -- stop (and lose) one thing
-      -- period = chTempoPeriod disp
-
-      chAll = replaceAll disp         -- change everything
+  let chAll = replaceAll disp         -- change everything
       hush = replaceAll disp M.empty  -- stop (and lose) everything
       off = killThread tid >> freeAll -- kill the program
-
 
       p1 = stack a b & dur .~ 4 where
         a = mmho 3 $ pre2 "a" [(0, m1 "freq" 0)
@@ -33,39 +29,33 @@ main = do
         b = mmho 2 $ pre2 "b" [(0, m1 "on" 0)
                               ,(1, m1 "freq" 4)]
 
-      run :: Int -> Museq String Msg = \k ->
-        mmho (fromIntegral k) $ pre2 "a" $
+      p2 :: Int -> Museq String Msg = \n ->
+        mmho (fromIntegral n) $ pre2 "a" $
         zip (map RTime [0..]) $
         map (M.singleton "freq" . (+) 0) $
-        map fromIntegral [0..k-1]
-      
-      chord :: [Float] -> Museq String Msg =
-        mmho 1 . pre2 "a" .
-        map (\f -> (0,M.singleton "freq" f))
-      
-      ampSeq :: Float -> Museq String Msg = \f ->
-        mmho 1 [("a",0,m1 "amp" f)]
+        map fromIntegral [0..n-1]
       
       go = nBoop . toHz . rootScale rs where
         toHz = ops [("freq", (*) 200 . \p -> 2**(p/12))]
         rs = slow 12 $ mmh 3 $ pre2 "a" $ [ (0, (0, phr3))
                                           , (1, (4, lyd))
-                                          , (2, (1, lyd7))
+                                          , (2 :: RTime, (1 :: Float, lyd7))
                                           ]
       
       wave = slow 4 $ mmh 2 $ pre2 "a" $ zip (map RTime [0..]) $
              [id, early $ 1]
-      
+
   chAll $ mfl [
-    ("1", go $ fast 2 $ merge0fa (slow 4 $ run 4) $ merge0fa (run 4) p1)
+    ("1", go $ fast 2 $ merge0fa (slow 4 $ p2 4) $ merge0fa (p2 4) p1)
     , ("2", go $ fast 4 $ meta wave $
-            merge0fa (slow 8 $ run 4) $ merge0fa (run 3) $
-            cat [p1, dense 2 p1])
-    , ("3", go $ merge0fa (merge0 (ampSeq 0.001) $ chord [10,12]) $
-            merge0fa (slow 4 $ run 4) $ merge0fa (run 4) p1)
+            merge0fa (slow 8 $ p2 4) $ merge0fa (p2 3) $
+            cat [p1, p2 3, dense 2 p1])
+      -- if I delete the "p2 3" from this last line,
+      -- it takes effect immediately. (Using two instances of append
+      -- instead of one cat has no effect.)
     ]
 
-  _ :: String <- readLn
+  _ <- getLine
   hush
   off
-  wait 1 -- give "hush" and "off" time to work
+
--- src/VividDispatch.hs
+++ src/VividDispatch.hs
@@ -1,6 +1,6 @@
 module VividDispatch (module M) where
 
-import Hode as M
+--import Hode as M
 import Dispatch.Types as M
 import Dispatch.Abbrevs as M
 import Dispatch.Transform as M
@@ -12,7 +12,7 @@ import Dispatch.Internal.Join as M
 import Dispatch.Dispatch as M
 import LongVarLists as M
 import Util as M
-import EarTrain as M
+--import EarTrain as M
 import Synths as M
 import FilepathSynonyms as M
 import Scale as M
@@ -20,11 +20,11 @@ import Synths.Config as M
 import Synths.Samples as M
 import Synths.Zot as M
 import Synths.Vap as M
-import Random.Types as M
-import Random.RandomSynth as M
-import Random.RandomSignal as M
-import Random.Types.Other as M
-import Random.Types.AbstractSignal as M
-import Random.Render as M
-import Random.MentionsSig as M
+--import Random.Types as M
+--import Random.RandomSynth as M
+--import Random.RandomSignal as M
+--import Random.Types.Other as M
+--import Random.Types.AbstractSignal as M
+--import Random.Render as M
+--import Random.MentionsSig as M
 import ReadHsAsGhci as M
