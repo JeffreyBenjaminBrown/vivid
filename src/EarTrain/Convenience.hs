a4eec9aae7f0909c57ea4e91d4b8e9ee92933f47
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-05-28 23:36:28 -0500

    rename a file

--- src/EarTrain/Convenience.hs
+++
@@ -1,8 +0,0 @@
-module EarTrain.Convenience where
-
-import EarTrain.EarTrain
-import EarTrain.Audio
-import EarTrain.Types
-
-
-maj46 n = earTrainFromScale 46 n [0,15,27]
---
+++ src/EarTrain/handy.hs
@@ -0,0 +1,19 @@
+-- module EarTrain.Convenience where
+
+import EarTrain.EarTrain
+import EarTrain.Audio
+import EarTrain.Types
+
+
+earTrainChromatic 31 2 31
+earTrainFromChordList 31 $ map (\n -> [0,n]) [1..3]
+
+earTrainFromScale 46 2 [0,15,27] -- the major chord
+
+
+playFreqs $ (*400) <$> [1,5/4+0.03,3/2-0.01]
+
+earTrainFromChordList 31 $ map (\n -> [0,n]) [25..29]
+earTrainFromChordList 31 $ map (\n -> [0,n]) [25,26]
+
+earTrainFromChordList 31 $ map (\n -> [0,n]) [7,8]
