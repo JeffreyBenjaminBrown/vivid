b903cf553322c82c39da92ea3dbf31bbc1d81469
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-07-02 19:20:03 -0500

    consolidate tests

--- mtv-test/Montevideo/Test/TMain.hs
+++ mtv-test/Montevideo/Test/TMain.hs
@@ -2,14 +2,20 @@ module Montevideo.Test.TMain where
 
 import Test.HUnit
 
---import Montevideo.Test.THode
 import Montevideo.Test.TDispatch
 import Montevideo.Test.TUtil
+import Montevideo.Monome.Test.Misc
+import Montevideo.Monome.Test.Sustain
+import Montevideo.Monome.Test.Windows
+import Montevideo.Monome.Test.JI
 
 
 tests :: IO Counts
 tests = runTestTT $ TestList [
-    TestLabel "test_module_dispatch" test_module_dispatch
---  , TestLabel "test_module_hode" test_module_hode
-  , TestLabel "test_module_util" test_module_util
+    test_module_dispatch
+  , test_module_util
+  , Montevideo.Monome.Test.Misc.tests
+  , Montevideo.Monome.Test.Windows.tests
+  , Montevideo.Monome.Test.Sustain.tests
+  , Montevideo.Monome.Test.JI.tests
   ]
