071700ffefbf5ca324330c45bd3fd435752bcfc6
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-07-11 13:50:40 -0500

    docs

--- mtv-lang/Montevideo/Dispatch/Config.hs
+++ mtv-lang/Montevideo/Dispatch/Config.hs
@@ -3,5 +3,6 @@ module Montevideo.Dispatch.Config where
 import Montevideo.Dispatch.Types
 
 
+-- | Half a frame after a synth is silenced, it is deleted.
 frameDuration :: Time
 frameDuration = 0.1
---
+++ mtv-lang/Montevideo/Random/README.md
@@ -0,0 +1,2 @@
+This code is imported nowhere.
+I guess I was using it from GHCI for something.
