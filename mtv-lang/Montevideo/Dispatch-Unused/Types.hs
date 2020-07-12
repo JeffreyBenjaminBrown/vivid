4c672e923511f09c461e7aa3f470feb0931fbee1
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-07-11 19:07:12 -0500

    Change some names in some unused code   to avoid a name conflict in the code that is used

--- mtv-lang/Montevideo/Dispatch-Unused/Types.hs
+++ mtv-lang/Montevideo/Dispatch-Unused/Types.hs
@@ -1,36 +1,36 @@
-data SynthRegister = -- per-synth boilerplate
-  SynthRegister { boops :: MVar (M.Map SynthName (Synth BoopParams))
-                , vaps  :: MVar (M.Map SynthName (Synth VapParams))
-                , sqfms :: MVar (M.Map SynthName (Synth SqfmParams))
+data SynthRegister' = -- per-synth boilerplate
+  SynthRegister' { boops' :: MVar (M.Map SynthName (Synth BoopParams))
+                 , vaps'  :: MVar (M.Map SynthName (Synth VapParams))
+                 , sqfms' :: MVar (M.Map SynthName (Synth SqfmParams))
                 -- , zots :: MVar (M.Map SynthName (Synth ZotParams))
                 }
 
-emptySynthRegister :: IO SynthRegister
-emptySynthRegister = do x <- newMVar M.empty
-                        y <- newMVar M.empty
-                        z <- newMVar M.empty
---                        w <- newMVar M.empty
-                        return $ SynthRegister x y z -- w
+emptySynthRegister' :: IO SynthRegister
+emptySynthRegister' = do x <- newMVar M.empty
+                         y <- newMVar M.empty
+                         z <- newMVar M.empty
+--                         w <- newMVar M.empty
+                         return $ SynthRegister x y z -- w
 
 -- | todo : this blocks if any MVar is empty
-showSynthRegister :: SynthRegister -> IO String
-showSynthRegister reg = do bs <- show <$> (readMVar $ boops reg)
-                           vs <- show <$> (readMVar $ vaps reg )
-                           ss <- show <$> (readMVar $ sqfms reg)
-                           return $ bs ++ "\n" ++ vs ++ "\n" ++ ss
+showSynthRegister' :: SynthRegister -> IO String
+showSynthRegister' reg = do bs <- show <$> (readMVar $ boops reg)
+                            vs <- show <$> (readMVar $ vaps reg )
+                            ss <- show <$> (readMVar $ sqfms reg)
+                            return $ bs ++ "\n" ++ vs ++ "\n" ++ ss
 
-data Dispatch = Dispatch {
-  mTimeMuseqs :: MVar (M.Map MuseqName (Time, Museq Action))
+data Dispatch' = Dispatch' {
+  mTimeMuseqs' :: MVar (M.Map MuseqName (Time, Museq Action))
     -- ^ Each `Time` here is the next time that Museq is scheduled to run.
     -- Rarely, briefly, those `Time` values will be in the past.
-  , reg :: SynthRegister
-  , mTime0 :: MVar Time
-  , mTempoPeriod :: MVar Duration
+  , reg' :: SynthRegister
+  , mTime0' :: MVar Time
+  , mTempoPeriod' :: MVar Duration
   }
 
 -- | "new" because it's not really empty, except for `time0`
-newDispatch :: IO Dispatch
-newDispatch = do
+newDispatch' :: IO Dispatch
+newDispatch' = do
   mTimeMuseqs <- newMVar M.empty
   reg <- emptySynthRegister
   mTime0 <- newEmptyMVar
