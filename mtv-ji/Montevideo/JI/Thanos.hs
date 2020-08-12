fe9f1421b8b43940db4615751a8196595787be31
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-08-12 18:46:25 -0500

    Replace some data with a function   -importantNotes = M.fromList   -  [ (87, [ (28, 5  % 4)   -        , (40, 11 % 8)             ...  +importantNotes :: Integer        -- ^ An EDO system.  +               -> [ ( Integer    -- ^ A value from that EDO system.  +                    , Rational)] -- ^ The ratio the EDO value represents.

--- mtv-ji/Montevideo/JI/Thanos.hs
+++ mtv-ji/Montevideo/JI/Thanos.hs
@@ -25,12 +25,13 @@ thanos'' :: Int -- ^ Make this `n` to keep every `n`th fret.
          -> Int -- ^ The maximum number of frets you're willing to stretch.
          -> IO ()
 thanos'' modulus edo maxStretch = let
-  spacings = filter (feasibleSpacing modulus) [5 .. 40]
-  r1 = [ (s, thanos' modulus s edo)
+  spacings = filter (feasibleSpacing modulus) [5 .. 50]
+  r1 = [ (s, thanos' modulus s $ fromIntegral edo)
        | s <- spacings ]
   r2 = filter ((<= maxStretch) . fst . snd) r1
   in mapM_ (\(spacing, (maxFretDiff, formatted)) -> do
-              putStrLn $ "\n" ++ show spacing
+              putStrLn $ "\nmod " ++ show modulus
+                ++ ", spaced " ++ show spacing ++ "\\" ++ show edo
                 ++ ", max reach: " ++ show maxFretDiff
                 ++ ", or in 12-edo, "
                 ++ show ( fromIntegral (12 * maxFretDiff * modulus)
@@ -39,7 +40,8 @@ thanos'' modulus edo maxStretch = let
      r2
 
 thanos' modulus spacing edo = let
-  notes = importantNotes M.! edo
+  notes :: [(Int, Rational)] =
+    importantNotes edo & traversed . _1 %~ fromIntegral
   maxFretDiff = maximum results' - minimum results'
     where results' = 0 : map snd results
   formatted = zip notes results
@@ -56,39 +58,19 @@ thanos :: Int -- ^ For the Kite tuning, this would be 2
        -> ( Int -- ^ which string to play it on
           , Int ) -- ^ which fret to play it on
 thanos modulus spacing edoStep =
-  let spaces = zip [0..] $ fmap (*spacing) [0..8]
+  let spaces = zip [0..] $ fmap (*spacing) [0..20]
       a = fmap (_2 %~ (edoStep -)) spaces
       b = filter ((== 0) . flip mod modulus . snd) a
       (string,fret) = minimumBy (comparing (abs . snd)) b
   in (string, div fret modulus)
 
 feasibleSpacing :: Int -> Int -> Bool
-feasibleSpacing modulus spacing = 
+feasibleSpacing modulus spacing =
   elem 1 $ fmap (flip mod modulus . (*) spacing) [1..modulus]
 
-importantNotes = M.fromList
-  [ (87, [ (28, 5  % 4)
-        , (40, 11 % 8)
-        , (51, 3  % 2)
-        , (61, 13 % 8)
-        , (70, 7  % 4)
-        , (87, 2  % 1) ] )
-  , (58, [ (19, 5 / 4 )
-         , (27, 11 / 8)
-         , (34, 3 / 2)
-         , (41, 13 / 8)
-         , (47, 7 / 4)
-         , (58, 2 ) ] )
-  , (46, [ (15, 5 % 4)
-         , (27, 3 % 2)
-         , (21, 11 % 8)
-         , (32, 13 % 8)
-         , (37, 7 % 4)
-        ,  (46, 2) ] )
-  , (41, [ (13, 5 / 4)
-         , (19, 11 / 8)
-         , (24, 3/2)
-         , (29, 13/8)
-         , (33, 7/4)
-         , (41, 2/2) ] )
-  ]
+importantNotes :: Integer        -- ^ An EDO system.
+               -> [ ( Integer    -- ^ A value from that EDO system.
+                    , Rational)] -- ^ The ratio the EDO value represents.
+importantNotes edo = let
+  primes = [5/4, 11/8, 3%2, 13/8, 7/4, 2]
+  in zip (map ((^. _1) . best edo) primes) primes
