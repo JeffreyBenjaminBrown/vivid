1be6219968db04f3c37688e1718361fff8c4bf62
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-08-16 13:41:31 -0500

    Monome: record some configurations I like.
      Includes the miracle 87-edo Thanos tuning,
      which discovery came from the Thanos code.

--- mtv-monome/Montevideo/Monome/Config.hs
+++ mtv-monome/Montevideo/Monome/Config.hs
@@ -9,11 +9,19 @@ import Montevideo.Monome.Types.EdoConfig
 
 -- | Change these values to tune the synth.
 -- See comments in Montevideo.Monome.Types.EdoConfig to see what they do.
+-- Some tunings I like:
+-- 22 1 5  -1.1
+-- 31 1 6   0.5
+-- 41 1 8   0
+-- 41 2 13  0 (the Kite Guitar tuning)
+-- 46 1 9   0
+-- 87 2 17  0
+
 theConfig :: EdoConfig
 theConfig = EdoConfig
-  { _edo = 87
-  , _spacing = 17
-  , _skip = 2
+  { _edo = 46
+  , _skip = 1
+  , _spacing = 9
   , _octaveStretchInCents = 0
   }
 
