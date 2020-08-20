5e3284154946b76a9ebc4ad820ff68f15f8ce1bb
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-08-19 21:14:49 -0500

    include all of Monome.Types.* in Monome.Types

--- mtv-monome/Montevideo/Monome/Types.hs
+++ mtv-monome/Montevideo/Monome/Types.hs
@@ -1,11 +1,20 @@
 module Montevideo.Monome.Types
-  ( module Montevideo.Monome.Types.Most
+  ( module Montevideo.Monome.Types.Device
+  , module Montevideo.Monome.Types.EdoConfig
+
   -- The instances defined in Montevideo.Monome.Types.Instances
   -- are all for types defined in Montevideo.Monome.Types.Most,
   -- so once Montevideo.Monome.Types.Most is exported,
   -- there is no need to export Montevideo.Monome.Types.Instances,
   -- and in fact doing so elicits a warning from GHC.
+  -- module Montevideo.Monome.Types.Instances
+
+  , module Montevideo.Monome.Types.Monome
+  , module Montevideo.Monome.Types.Most
   ) where
 
-import Montevideo.Monome.Types.Most
+import Montevideo.Monome.Types.Device
+import Montevideo.Monome.Types.EdoConfig
 import Montevideo.Monome.Types.Instances()
+import Montevideo.Monome.Types.Monome
+import Montevideo.Monome.Types.Most
