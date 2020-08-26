9a0d3f107347c3780046f4752e9eacd5e4776336
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-08-26 18:55:37 -0500

    Replace an ugly switch statement with something prettier.
      +inverseShowMonome :: String -> Either String MonomeId

--- mtv-monome/Montevideo/Monome/Util/Button.hs
+++ mtv-monome/Montevideo/Monome/Util/Button.hs
@@ -1,4 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings
+, ViewPatterns
+#-}
 
 module Montevideo.Monome.Util.Button (
     X, Y, Switch, Led, LedBecause(..)
@@ -30,9 +32,9 @@ boolFromInt x = Left ( "boolFromInt: " ++ show x
 -- Right ((7,7),True)
 readOSC_asSwitch :: OSC -> Either String ( MonomeId
                                          , ((X,Y), Switch))
-readOSC_asSwitch m@(OSC s l) =
+readOSC_asSwitch m@(OSC str l) =
   mapLeft ("readOSC_asSwitch" ++) $
-  let ms :: [String] = lines' '/' $ unpack s
+  let ms :: [String] = lines' '/' $ unpack str
       err = Left $ "Unrecognized OSC message: " ++ show m
   in case l of
        [OSC_I x, OSC_I y, OSC_I s] -> do
@@ -41,15 +43,17 @@ readOSC_asSwitch m@(OSC s l) =
            [t1,t2,t3] ->
              if t2 == "grid" && t3 == "key"
              then let press = ((fi x, fi y), b)
-                  in if t1 == show Monome_128
-                     then Right (Monome_128, press)
-                     else if t1 == show Monome_256
-                          then Right (Monome_256, press)
-                          else err
+                  in case inverseShowMonome t1 of
+                       Right monome -> Right (monome, press)
+                       Left _ -> err
              else err
            _ -> err
        _ -> err
 
+inverseShowMonome :: String -> Either String MonomeId
+inverseShowMonome ((==) (show Monome_256) -> True) = Right Monome_256
+inverseShowMonome ((==) (show Monome_128) -> True) = Right Monome_128
+inverseShowMonome m = Left $ "Monome not found: " ++ m ++ "."
 
 -- | Tells the monome to turn on an LED. See Test/HandTest.hs.
 ledOsc :: MonomeId -> ((X,Y), Led) -> ByteString
