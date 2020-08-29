95e026cbb627467fd035633f07df5a0617815048
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-08-28 21:44:44 -0500

    correction: add a file I shold have added two commits ago   at 31f15df44ddb6ccd14010ef3288b059c8a00a4f9

---
+++ mtv-monome/Montevideo/Monome/Window/Lag.hs
@@ -0,0 +1,36 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# LANGUAGE ScopedTypeVariables
+, TypeApplications
+#-}
+
+module Montevideo.Monome.Window.Lag (
+    handler
+  , lagWindow
+  , label
+  ) where
+
+import           Control.Lens
+
+import           Montevideo.Monome.Types.Most
+import           Montevideo.Util
+
+
+label :: WindowId
+label = LagWindow
+
+lagWindow :: Window EdoApp
+lagWindow =  Window {
+    windowLabel = label
+  , windowContains = \(x,y) -> numBetween 0 15 x &&
+                               y == 0
+  , windowInitLeds = \_ _ -> []
+  , windowHandler = handler }
+
+-- TODO : magic numbers; reify
+handler :: St EdoApp -> (MonomeId, ((X,Y), Switch))
+        -> Either String (St EdoApp)
+handler    st           (_, ((x,_),_)) =
+  Right $ st & stLag .~ ( logScale (0,15)
+                          ( 0.03, 10 )
+                          $ fi x )
+  
