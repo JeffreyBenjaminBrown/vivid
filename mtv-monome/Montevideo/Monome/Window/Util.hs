4d562439e78c27e71979c3f868533aa723637702
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-09-20 15:14:21 -0500

    nothing
      Reorder code.
      Rename some variables.

--- mtv-monome/Montevideo/Monome/Window/Util.hs
+++ mtv-monome/Montevideo/Monome/Window/Util.hs
@@ -1,14 +1,13 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Montevideo.Monome.Window.Util (
-  -- | * only exported for the sake of testing
-  belongsHere    -- ^ [Window] -> Window -> LedFilter
-
-  , LedRelay, LedFilter
-  , relayToWindow -- ^ St -> WindowId -> [Window] -> LedRelay
-  , relayIfHere   -- ^ Socket > [Window] -> Window -> LedRelay
-
+    LedRelay, LedFilter
   , nextVoice -- ^ M.Map VoiceId a -> VoiceId
+  , relayToWindow -- ^ St -> WindowId -> [Window] -> LedRelay
+
+  -- | * only exported for the sake of testing
+  , relayIfHere   -- ^ Socket > [Window] -> Window -> LedRelay
+  , belongsHere    -- ^ [Window] -> Window -> LedFilter
   ) where
 
 import           Prelude hiding (pred)
@@ -22,9 +21,14 @@ import Montevideo.Monome.Util.OSC
 
 
 -- | Forward a message to the monome if appropriate.
--- These are only used in this module.
 type LedRelay  = ((X,Y), Led) -> IO ()
 type LedFilter = (X,Y) -> Bool
+
+nextVoice :: St a -> VoiceId
+nextVoice st =
+  case M.lookupMax $ _stVoices st of
+    Nothing             -> VoiceId 0
+    Just (VoiceId i, _) -> VoiceId $ i+1
 
 relayToWindow :: St app -> MonomeId -> WindowId -> Either String LedRelay
 relayToWindow st mi wl =
@@ -40,6 +44,9 @@ relayToWindow st mi wl =
     Right $ M.lookup mi $ _stToMonome st
   Right $ relayIfHere (sock, mi) ws w
 
+
+-- * only exported for the sake of testing
+
 -- | `relayIfHere dest ws w` returns a `LedRelay` which,
 -- if the coordinate falls in `w` and in no other `Window` before `w` in `ws`,
 -- sends the message to the `Socket`.
@@ -47,23 +54,18 @@ relayIfHere :: (Socket, MonomeId) -> [Window app] -> Window app -> LedRelay
 relayIfHere (dest, mi) ws w = f where
   f :: ((X,Y),Led) -> IO ()
   f msg = if belongsHere ws w $ fst msg
-    then (send dest $ ledOsc mi msg) >> return ()
-    else return ()
+          then do send dest $ ledOsc mi msg
+                  return ()
+          else return ()
 
 -- | `belongsHere allWindows w _` returns an `LedFilter` that returns `True`
 -- if `(X,Y)` belongs in `w` and none of the `Window`s preceding `w`.
 -- PITFALL: `allWindows` should include literally all of them, even `w`.
 belongsHere :: [Window app] -> Window app -> LedFilter
 belongsHere allWindows w = f where
-  obscurers = takeWhile (/= w) allWindows
-    -- `obscurers` == the windows above `w`
+  higherWindows = takeWhile (/= w) allWindows
   obscured :: (X,Y) -> Bool
-  obscured xy = or $ map ($ xy) $ map windowContains obscurers
+  obscured xy = or $
+    map (($ xy) . windowContains) higherWindows
   f :: (X,Y) -> Bool
   f btn = not (obscured btn) && windowContains w btn
-
-nextVoice :: St a -> VoiceId
-nextVoice st =
-  case M.lookupMax $ _stVoices st of
-    Nothing             -> VoiceId 0
-    Just (VoiceId i, _) -> VoiceId $ i+1
