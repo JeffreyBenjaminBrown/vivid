9dce843fb7e98d7e53a60dd9b452d530cf687c25
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2020-09-20 21:37:12 -0500

    bugfix in notObscured   To convert from relative coordinate system S to system T,   must *add* the origin from S and *subtract* the one from S.   I was doing the reverse. rename    notObscured -> visible

--- mtv-monome/Montevideo/Monome/Window/Util.hs
+++ mtv-monome/Montevideo/Monome/Window/Util.hs
@@ -8,7 +8,7 @@ module Montevideo.Monome.Window.Util (
 
   -- | * only exported for the sake of testing
   , relayIfHere
-  , notObscured
+  , visible
   ) where
 
 import           Prelude hiding (pred)
@@ -65,25 +65,25 @@ relayIfHere :: Socket
   -> LedRelay
 relayIfHere dest allWindows w = f where
   f :: ((MonomeId, WindowId), ((X,Y), Led)) -> IO ()
-  f ((mi,wi), (xy,led)) =
-    if notObscured allWindows w xy
-    then do send dest $ ledOsc mi
-              ( pairAdd xy $ fst w -- absolute monome coordinates
-              , led )
+  f ((mi,_), (xy,led)) = -- the WindowId is no longer needed, given w
+    if visible allWindows w xy
+    then do _ <- send dest $ ledOsc mi
+                 ( pairAdd xy $ fst w -- absolute monome coordinates
+                 , led )
             return ()
     else return () -- do nothing
 
--- | `notObscured allWindows w _` returns an `LedFilter` that returns `True`
+-- | `visible allWindows w _` returns an `LedFilter` that returns `True`
 -- if `(X,Y)` belongs in `w` and none of the `Window`s preceding `w`.
 --
 -- PITFALL: The LedMsg's (X,Y) coordinates are relative to `w`.
 -- PITFALL: `allWindows` should include literally all of them, even `w`.
 
-notObscured :: forall app.
+visible :: forall app.
      [((X,Y), Window app)] -- ^ All Windows, in order, with top-left corners.
   ->  ((X,Y), Window app)  -- ^ The Window onto which we might draw.
   -> LedFilter
-notObscured allWindows (wtl, w) = f where
+visible allWindows (wtl, w) = f where
   f :: (X,Y) -> Bool
   f led = not (obscured led) && windowContains w led
   obscured :: (X,Y) -- the Led, relative to w
@@ -93,8 +93,8 @@ notObscured allWindows (wtl, w) = f where
            -> ((X,Y), Window app) -- a Window that might block the Led
            -> Bool
   obscures wRelative (wtl', w') = let
-    absolute = pairSubtract wRelative wtl
-    w'Relative = pairAdd absolute wtl' -- the Led, relative to w'
+    absolute = pairAdd wRelative wtl
+    w'Relative = pairSubtract absolute wtl' -- the Led, relative to w'
     in windowContains w' w'Relative
   higherWindows :: [((X,Y), Window app)] = -- Windows that might block the Led
     takeWhile ((/= w) . snd) allWindows
