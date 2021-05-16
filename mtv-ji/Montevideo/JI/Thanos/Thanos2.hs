bf04b2a3a64a79e1e62c3dfb0c1c9342bf0821c5
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2021-05-15 19:08:44 -0500

    Thanos tuning search:
      Report more layout statistics in the Area type.

--- mtv-ji/Montevideo/JI/Thanos/Thanos2.hs
+++ mtv-ji/Montevideo/JI/Thanos/Thanos2.hs
@@ -24,7 +24,14 @@ type Tuning = ( Edo
               , EdoInterval ) -- ^ the fret gap
 type LayoutRow = (EdoInterval, Rational, GString, GFret)
 type Layout = [LayoutRow]
-type Area = Int
+
+-- | This used to just be the area; now it includes a few other statistics.
+-- The name ought to be different,
+-- but that would complicate the names of some functions that use it.
+type Area = ( Int   -- ^ area
+            , Int   -- ^ string width
+            , Int   -- ^ fret width
+            , Float ) -- ^ freat where octave lies, as a %
 
 -- | For positive ratios, gives the nearest (string, fret)
 -- position of the ratio such that the fret is positive.
@@ -69,14 +76,20 @@ layouts tuning@(edo, stringGap, fretGap) ratios =
 bestLayout :: Tuning -> [Rational] -> (Area, Layout)
 bestLayout tuning @ (edo, stringGap, fretGap) ratios =
   let ls = layouts tuning ratios
-      area :: Layout -> Int
+      area :: Layout -> Area
       area l = let
         strings = map (^. _3) l
         frets = map (^. _4) l
-        in (2 + maximum strings - minimum strings)
-        -- PITFALL: Hard-coded parameter.
-        -- Adding something (I like adding 2) to the string width penalizes wide frets more than wide strings. Relevant for guitar, not for keyboard.
-           * (maximum frets - minimum frets)
+        stringWidth = maximum strings - minimum strings
+        fretWidth = maximum frets - minimum frets
+        octaveFret = fi ((l !! 1 ^. _4) - minimum frets)
+                     / fi fretWidth
+        area = if stringWidth > 7 then 9999999 else
+                 (2 +
+                  -- PITFALL: Hard-coded parameter.
+                  -- Adding something like 2 to the string width penalizes wide frets more than wide strings. Relevant for guitar, not for keyboard.
+                  stringWidth) * fretWidth
+        in (area, stringWidth, fretWidth, octaveFret)
       l = L.minimumBy (comparing area) ls
   in (area l, l)
 
@@ -86,7 +99,9 @@ tunings :: Edo -> [Tuning]
 tunings edo = let e = fi edo in
   [ ( edo, stringGap, fretGap ) |
     fretGap <- [ 1 .. round (e * (5/12) ) ],
-    stringGap <- [ round (e / 6) .. round (e * (5/12) ) ],
+    stringGap <- [ fretGap .. round (e * (5/12) ) ],
+      -- sometimes I start it from `(round $ e / 6)`
+      -- rather than `fretGap`
     fretGap < stringGap,
     relativelyPrime fretGap stringGap
     -- The rest of this is to keep it guitar-friendly
@@ -123,7 +138,7 @@ bestEdoLayouts rs es  = let
 data EdoTuningReport = EdoTuningReport {
   etrEdo :: Edo,
   etrTuning :: Tuning',
-  etrArea :: Area,
+  etrArea :: Area',
   etrLayout :: [LayoutRow'] }
   deriving (Show, Eq, Ord)
 
@@ -132,7 +147,7 @@ mkEdoTuningReport :: (Edo, Tuning, Area, Layout)
 mkEdoTuningReport (edo,tuning,area,layout) =
   EdoTuningReport { etrEdo = edo,
                     etrTuning = Tuning' tuning,
-                    etrArea = area,
+                    etrArea = Area' area,
                     etrLayout = map LayoutRow' layout }
 
 data Tuning' = Tuning' Tuning
@@ -143,6 +158,14 @@ instance Show Tuning' where
     "; StringGap " ++ show sg ++
     "; FretGap "   ++ show fg
 unTuning (Tuning' t) = t
+
+data Area' = Area' Area
+  deriving (Eq, Ord)
+instance Show Area' where
+  show (Area' (area, sw, fw, oct)) =
+    "strings x frets: " ++ show sw ++ " x " ++ show fw ++
+    ", octave fraction: " ++ show oct
+unArea (Area' a) = a
 
 data LayoutRow' = LayoutRow' LayoutRow
   deriving (Eq, Ord)
